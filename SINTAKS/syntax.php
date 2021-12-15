cf839bc662376c211442bb83a3db75be1c92938f
seeyalater161201 <79683734+seeyalater161201@users.noreply.github.com>
2021-12-15 13:58:20 +0700

    Add files via upload

---
+++ SINTAKS/syntax.php
@@ -0,0 +1,85 @@
+<?php 
+// Standar Output
+// echo, print
+// print_r (khusus array)
+// var_dump (melihat isi variable)
+
+// echo "Aryudha Willy";
+// print_r ("Aryudha Willy");
+// var_dump("Aryudha Willy");
+// echo 161201;
+
+// echo true;
+// echo false;
+
+// Penulisan sintaks php
+// 1. php dalam html
+// 2. html dalam php
+
+// variable dan tipe data
+// variable
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Aryudha";
+// penulisan interpolasi
+// echo "Halo, Nama saya $nama";
+
+// Operator
+// aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x + $y;
+
+
+// Operator penggabung string (concat)
+// .
+// $nama_depan = "Aryudha";
+// $nama_belakang = "Willy";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, =+, -=, *=, /=, %=, .=
+// $x = 5;
+// $x += 10;
+// echo $x;
+
+// $nama = "Aryudha";
+// $nama .= " ";
+// $nama .= "Willy";
+// echo $nama;
+
+// Perbandingan
+//  <, >, <=, >=, ==, !=
+// var_dump(1 > 5);
+// var_dump(1 == "1");
+
+// Identitas
+// ===, !==
+// var_dump(1 === "1");
+
+// Logika
+// &&, ||, !
+// $x = 10;
+// var_dump($x < 20 && $x % 2 == 0);
+// var_dump($x < 30 || $x % 2 == 0);
+
+?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Halo, Selamat datang <?php echo $nama;?>
+    </h1>
+
+    <p><?php echo"ini paragraf"; ?>
+    </p>
+
+
+</body>
+</html> -->
+
